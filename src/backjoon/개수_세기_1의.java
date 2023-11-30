2620a28b607271a18bde07ff8441cf31fff936c3
JeongMin <yooa.dev@gmail.com>
2023-11-30 15:40:29 +0900

    231129 백준 : 9527 1의 개수 세기_비트 연산으로 해결

--- src/backjoon/개수_세기_1의.java
+++ src/backjoon/개수_세기_1의.java
@@ -17,18 +17,30 @@ public class 개수_세기_1의 {
         long a = Long.parseLong(st.nextToken());
         long b = Long.parseLong(st.nextToken());
 
-        int cnt = 0;
-        for(long i = a; i <= b; i++) {
-            String binaryString = Long.toBinaryString(i);
-            for(int j = 0; j < binaryString.length(); j++) {
-                cnt += binaryString.charAt(j) - '0';
+        long[] dp = new long[55];
+        initSet(dp);
+
+        long result = calculateCount(b, dp) - calculateCount(a-1, dp);
+        System.out.println(result);
+    }
+
+    public static void initSet(long[] dp) {
+        dp[0] = 1;
+        for(int i = 1; i < 55; i++) {
+            dp[i] = dp[i-1] * 2 + (1L << i);
+        }
+    }
+    public static long calculateCount(long n, long[] dp) {
+        // n보다 작은 2^n의 최댓값 구하기
+        long count = n & 1; // n이 1이라면 count 초기화를 1로 해놓아야 한다. for문에서 카운팅 안된다.
+        int max = (int) (Math.log(n) / Math.log(2));
+        for(int i = max; i > 0; i--) {
+            if((n & (1L << i)) != 0L) {
+                count += dp[i-1] + (n - (1L << i) + 1);
+                n -= (1L << i); // 비트 이동시키기
             }
         }
 
-        System.out.println(cnt);
-
-        Runtime.getRuntime().gc();
-        long usedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-        System.out.print(usedMemory + " bytes");
+        return count;
     }
 }
